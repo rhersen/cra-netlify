b31feeb98543345dbfca2843636a3343cb2407a7
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-06-26 00:14:08 +0200

    Rename Departures to App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
-import Departures from "./Departures"
+import Table from "./Table"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>stations</p>
-        <Departures />
-      </header>
-    </div>
-  )
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { departures: [], msg: "", now: new Date() }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.button("Sub")}
+        {this.button("Cst")}
+        {this.button("Flb")}
+        {this.button("Tul")}
+        <div>{this.state.msg}</div>
+        <Table announcements={this.state.departures} now={this.state.now} />
+      </div>
+    )
+  }
+
+  button(location) {
+    return (
+      <button
+        onClick={async () => {
+          const response = await fetch(
+            `/.netlify/functions/node-fetch?location=${location}`
+          )
+          const json = await response.json()
+          if (json.msg) this.setState({ msg: json.msg })
+          if (json.TrainAnnouncement)
+            this.setState({ departures: json.TrainAnnouncement, msg: "" })
+        }}
+      >
+        {location}
+      </button>
+    )
+  }
 }
-
-export default App
