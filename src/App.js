ff57a2f8f0dc0abaa71efcf4fe1d13c64386c960
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-07-04 07:19:00 +0200

    Remove logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
-import Departures from "./Departures"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Departures />
-      </header>
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { departures: [], msg: "" }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.button("Sub")}
+        {this.button("Cst")}
+        {this.button("Flb")}
+        {this.button("Tul")}
+        <div>{this.state.msg}</div>
+        <ul>
+          {this.state.departures
+            .filter(d => d.ToLocation)
+            .map(d => (
+              <li key={d.AdvertisedTrainIdent}>
+                {d.TypeOfTraffic} {d.AdvertisedTrainIdent} mot{" "}
+                {d.ToLocation.map(loc => loc.LocationName)}{" "}
+                {d.TimeAtLocation ? "avgick" : "avgår"} från spår{" "}
+                {d.TrackAtLocation} kl{" "}
+                {d.TimeAtLocation
+                  ? d.TimeAtLocation.substr(11, 5)
+                  : d.AdvertisedTimeAtLocation.substr(11, 5)}
+              </li>
+            ))}
+        </ul>
+      </div>
+    )
+  }
+
+  button(location) {
+    return (
+      <button
+        onClick={async () => {
+          const response = await fetch(
+            `/.netlify/functions/node-fetch?location=${location}`
+          )
+          const json = await response.json()
+          if (json.msg) this.setState({ msg: json.msg })
+          if (json.TrainAnnouncement)
+            this.setState({ departures: json.TrainAnnouncement, msg: "" })
+        }}
+      >
+        {location}
+      </button>
+    )
+  }
 }
 
 export default App
