80ad3ca3c795631aec230b9f85ffd26c8e5f8b12
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-08-20 22:08:18 +0200

    Add FromLocation

--- src/Destination.js
+++ src/Destination.js
@@ -4,6 +4,13 @@ import locations from "./locations"
 export default function Destination({ announcement }) {
   return (
     <td className="destination">
+      <span className="from">
+        {announcement.FromLocation &&
+          announcement.FromLocation.map(location =>
+            locations(location.LocationName)
+          ).join()}
+        –
+      </span>
       {announcement.ToLocation &&
         announcement.ToLocation.map(location =>
           locations(location.LocationName)
