3fa139c17fab6ef68fd1093d9d631e2260f5e779
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-09-06 20:39:33 +0200

    Refresh on focus

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import difference_in_seconds from "date-fns/difference_in_seconds"
 import "./App.css"
 import Nav from "./Nav"
 import Sheet from "./Sheet"
@@ -11,8 +12,28 @@ class App extends React.Component {
       msg: "",
       announcements: [],
       direction: undefined,
-      branch: undefined
+      branch: undefined,
+      then: undefined
     }
+  }
+
+  componentDidMount() {
+    window.addEventListener("focus", this.onFocus)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("focus", this.onFocus)
+  }
+
+  onFocus = () => {
+    const { branch, direction } = this.state
+    if (branch && direction && this.secondsAgo() > 30)
+      this.getTrains(branch, direction)
+  }
+
+  secondsAgo() {
+    const { then } = this.state
+    return then ? difference_in_seconds(Date.now(), then) : 0
   }
 
   render() {
@@ -22,18 +43,7 @@ class App extends React.Component {
         {msg && <div>{msg}</div>}
         <Nav
           getTrains={async ({ branch, direction }) => {
-            this.setState({
-              branch,
-              direction,
-              announcements: []
-            })
-            const response = await fetch(
-              `/.netlify/functions/node-fetch?direction=${direction}&locations=${location[branch]}`
-            )
-            const json = await response.json()
-            if (json.msg) this.setState({ msg: json.msg })
-            if (json.TrainAnnouncement)
-              this.setState({ announcements: json.TrainAnnouncement, msg: "" })
+            await this.getTrains(branch, direction)
           }}
         />
         <Sheet
@@ -46,6 +56,33 @@ class App extends React.Component {
         />
       </div>
     )
+  }
+
+  async getTrains(branch, direction) {
+    this.clearAnnouncements(branch, direction)
+    const response = await fetch(
+      `/.netlify/functions/node-fetch?direction=${direction}&locations=${location[branch]}`
+    )
+    const json = await response.json()
+    if (json.msg) this.setState({ msg: json.msg })
+    if (json.TrainAnnouncement) this.setAnnouncements(json)
+  }
+
+  setAnnouncements(json) {
+    this.setState({
+      announcements: json.TrainAnnouncement,
+      msg: "",
+      then: Date.now()
+    })
+  }
+
+  clearAnnouncements(branch, direction) {
+    this.setState({
+      branch,
+      direction,
+      announcements: [],
+      then: undefined
+    })
   }
 }
 
