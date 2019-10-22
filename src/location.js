a5ff1db79d944bce64eba5c9395af6e0d929f919
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-10-22 11:48:32 +0200

    Move Kmy to its correct place

--- src/location.js
+++ src/location.js
@@ -1,7 +1,7 @@
 // prettier-ignore
 export default {
   c: ["Äs", "Åbe", "Sst", "Sci", "Sod", "Tmö"],
-  n: ["So", "Kmy", "Udl", "Hel", "Sol", "Hgv", "Nvk", "R", "Upv", "Skby", "Rs", "Bra", "Mr"],
+  n: ["So", "Udl", "Hel", "Kmy", "Sol", "Hgv", "Nvk", "R", "Upv", "Skby", "Rs", "Bra", "Mr"],
   s: ["Öte", "Rön", "Gau", "Södy", "Tu", "Uts", "Tul", "Flb", "Hu", "Sta"],
   e: ["Hfa", "Ts", "Kda", "Vhe", "Jbo", "Hnd", "Vga", "Skg", "Tåd", "Fas", "Hön"],
   w: ["Huv", "Sub", "Duo", "Spå", "Bkb", "Jkb"],
