3ddffd5ddb18a39ab7e878ffb2e698cbefa225c1
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-12-16 16:00:47 +0100

    Show Kallhäll station too

--- src/location.js
+++ src/location.js
@@ -4,6 +4,6 @@ export default {
   n: ["So", "Udl", "Hel", "Kmy", "Sol", "Hgv", "Nvk", "R", "Upv", "Skby", "Rs", "Bra", "Mr"],
   s: ["Öte", "Rön", "Gau", "Södy", "Tu", "Uts", "Tul", "Flb", "Hu", "Sta"],
   e: ["Hfa", "Ts", "Kda", "Vhe", "Jbo", "Hnd", "Vga", "Skg", "Tåd", "Fas", "Hön"],
-  w: ["Huv", "Sub", "Duo", "Spå", "Bkb", "Jkb"],
+  w: ["Huv", "Sub", "Duo", "Spå", "Bkb", "Jkb", "Khä", "Kän"],
   u: ["Bvr", "Arne", "Arnc", "Myn", "Kn", "Eby", "Säy", "U"]
 }
